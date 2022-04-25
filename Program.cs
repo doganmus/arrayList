04c5d60bf2110dba2a0a7762fce1481b28c27581
Dogan Mus <doganmus87@gmail.com>
2022-04-25 23:40:27 +0300

    Ders Sonu

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace arrayList
 {
@@ -6,7 +8,53 @@ namespace arrayList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //System.Collections namespace
+
+            ArrayList liste = new ArrayList();
+            // liste.Add("Ayşe");
+            // liste.Add(2);
+            // liste.Add(true);
+            // liste.Add('A');
+
+            //içerisindeki verilere erişim
+            // Console.WriteLine(liste[1]);
+            // foreach (var item in liste)
+            //     Console.WriteLine(item);
+            
+            //AddRange
+            Console.WriteLine("****** Add Range *****");
+            // string [] renkler = {"kırımız","sarı","yeşil"};
+            List<int> sayilar = new List<int>(){1,8,3,7,9,92,5};
+            //liste.AddRange(renkler);
+            liste.AddRange(sayilar);
+
+            foreach (var item in liste)
+                Console.WriteLine(item);
+            
+            //Sort
+            Console.WriteLine("***** Sort *****");
+            liste.Sort();
+
+            foreach (var item in liste)
+                Console.WriteLine(item);
+            
+            //Binary Search
+            Console.WriteLine("***** Binary Search *****");
+            Console.WriteLine(liste.BinarySearch(9));
+
+            //Reverse
+            Console.WriteLine("***** Reverse *****");
+            liste.Reverse();
+            foreach (var item in liste)
+                Console.WriteLine(item);
+            
+            //Clear
+            Console.WriteLine("***** Clear *****");
+            liste.Clear();
+            foreach (var item in liste)
+                Console.WriteLine(item);
+
+
         }
     }
 }
